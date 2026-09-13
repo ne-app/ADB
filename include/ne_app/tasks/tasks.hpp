489238c634e7babd51d51d897e53115787936fd1
Amlal El Mahrouss <amlal@nekernel.org>
2026-09-13 16:33:52 +0200

    FEAT: src: improvements over the tasks.hpp, introducing some needed concepts as well

--- include/ne_app/tasks/tasks.hpp
+++ include/ne_app/tasks/tasks.hpp
@@ -5,33 +5,52 @@
 #pragma once
 
 #include <atomic>
-#include <boost/asio/io_context.hpp>
-#include <boost/throw_exception.hpp>
 #include <functional>
-#include <ne_app/core/store.hpp>
-#include <stdexcept>
-#include <utility>
+#include <future>
 
 namespace ne_app::tasks {
 
 struct task_tag final {
-  std::atomic_flag tf_{ATOMIC_FLAG_INIT};
+  std::atomic_flag tf_{};
   task_tag() = default;
   ~task_tag() = default;
 };
 
 template <typename Args, typename... Args2>
-void run(const std::function<Args>& fn, task_tag& t, Args2... a) {
+void run(const std::function<Args>& fn, task_tag& t, Args2&&... a) {
   while (!t.tf_.test_and_set(std::memory_order_acquire));
 
   try {
     fn(t.tf_, a...);
   } catch (...) {
     t.tf_.clear(std::memory_order_release);
-    boost::throw_exception(std::runtime_error("task_error"));
+    return;
   }
 
   t.tf_.clear(std::memory_order_release);
+}
+
+/// @brief protocol definitions for 'await_or_error' type of return promises.
+enum class await_or_error {
+  kError = 588,
+  kNoAwait = kError,
+  kSuccess = 88,
+};
+
+/// @brief Await task first, then respond.
+template <typename RetProm, typename Args, typename... Args2>
+std::promise<RetProm> await_first(const std::function<Args>& fn, task_tag& t, Args2&&... a) {
+  while (!t.tf_.test_and_set(std::memory_order_acquire));
+
+  try {
+    co_await fn(t.tf_, a...);
+  } catch (...) {
+    t.tf_.clear(std::memory_order_release);
+    co_return RetProm::kError;
+  }
+
+  t.tf_.clear(std::memory_order_release);
+  co_return RetProm::kSuccess;
 }
 
 }  // namespace ne_app::tasks
